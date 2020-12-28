36deb4c057d1a02b36ee8ae3f7cbd03b78047aad
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-28 16:13:09 +0100

    write some tests

---
+++ tests/core.ts
@@ -0,0 +1,42 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { langauge } from '../src/index'
+
+const test = suite('core')
+
+const translation = {
+	NO_PARAM: 'This is a test.',
+	PARAM: '{0}',
+	TRIM_KEY: '{ 0 }',
+	TRIM_PARAM: 'Hey{0}',
+	PLURAL: 'apple{{s}}',
+	SINGULAR_PLURAL: '{0} {{Apfel|Äpfel}}',
+	MULTIPLE_PARAMS: '{0} {1}',
+	MULTIPLE_PARAMS_PLURAL: '{0} banana{{s}} and {1} apple{{s}}',
+}
+
+const LLL = langauge(translation)
+
+test('no param', () => assert.is(LLL.NO_PARAM(), translation.NO_PARAM))
+
+test('trim key', () => assert.is(LLL.TRIM_KEY('test'), 'test'))
+test('trim param', () => assert.is(LLL.TRIM_PARAM(' Ho '), 'HeyHo'))
+
+test('param', () => assert.is(LLL.PARAM('hi'), 'hi'))
+
+test('param but empty', () => assert.is(LLL.PARAM(), ''))
+
+test('plural 0', () => assert.is(LLL.PLURAL(0), 'apples'))
+test('plural 1', () => assert.is(LLL.PLURAL(1), 'apple'))
+test('plural 2', () => assert.is(LLL.PLURAL(2), 'apples'))
+
+test('singular-plural 0', () => assert.is(LLL.SINGULAR_PLURAL(0), '0 Äpfel'))
+test('singular-plural 1', () => assert.is(LLL.SINGULAR_PLURAL(1), '1 Apfel'))
+test('singular-plural 2', () => assert.is(LLL.SINGULAR_PLURAL(2), '2 Äpfel'))
+
+test('multiple params', () => assert.is(LLL.MULTIPLE_PARAMS(1, 2), '1 2'))
+
+test('multiple params plural', () => assert.is(LLL.MULTIPLE_PARAMS_PLURAL(1, 2), '1 banana and 2 apples'))
+
+test.run()
---
+++ tests/formatters.ts
@@ -0,0 +1,47 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { langauge } from '../src/index'
+import { uppercase, lowercase, replace, date } from '../src/formatters'
+
+const test = suite('formatters')
+
+const translation = {
+	UPPERCASE: 'This is a {0|uppercase}.',
+	MANUAL_UPPERCASE: 'I AM {0|manualUppercase}.',
+	LOWERCASE: 'This should be {0|lowercase}.',
+	CENSOR_NUMBERS: 'Your PIN is {0|censorNumbers}',
+	DATE_EN: 'The date is {0|dateEN}.',
+	DATE_DE: 'The date is {0|dateDE}.',
+	NO_SPACES: '{0|noSpaces}',
+}
+
+const formatters = {
+	uppercase,
+	manualUppercase: (value: string) => value.toUpperCase(),
+	lowercase,
+	censorNumbers: replace(/[0-9]/g, '*'),
+	dateEN: date('en'),
+	dateDE: date('de'),
+	noSpaces: (value: string) => value.replace(/\s/g, ''),
+}
+
+const LLL = langauge(translation, { formatters })
+
+const someDate = new Date('2020-03-13')
+
+test('uppercase', () => assert.is(LLL.UPPERCASE('test'), 'This is a TEST.'))
+
+test('manual uppercase', () => assert.is(LLL.MANUAL_UPPERCASE('big'), 'I AM BIG.'))
+
+test('lowercase', () => assert.is(LLL.LOWERCASE('SMALL'), 'This should be small.'))
+
+test('censor numbers', () => assert.is(LLL.CENSOR_NUMBERS('1234'), 'Your PIN is ****'))
+
+test('date en', () => assert.is(LLL.DATE_EN(someDate), 'The date is 3/13/2020.'))
+
+test('date de', () => assert.is(LLL.DATE_DE(someDate), 'The date is 2020-3-13.'))
+
+test('no spaces', () => assert.is(LLL.NO_SPACES('some text with no spaces'), 'sometextwithnospaces'))
+
+test.run()
