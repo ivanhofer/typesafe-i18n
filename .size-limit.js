11e0a44930574aa9ab771f74f3daa4b3c0a2b594
Ivan Hofer <ivan.hofer@outlook.com>
2021-01-06 17:25:21 +0100

    feat: add size-limit to be sure the ouput stays small

---
+++ .size-limit.js
@@ -0,0 +1,10 @@
+module.exports = [
+	{
+		path: 'dist/langauge.min.js',
+		limit: '798 b',
+	},
+	{
+		path: 'dist/index.min.js',
+		limit: '926 b',
+	},
+]
--- rollup-browser.config.ts
+++ rollup-browser.config.ts
@@ -2,39 +2,30 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
-const config = [
-	{
-		input: './src/index.ts',
-		output: [
-			{
-				file: 'dist/index.js',
-				format: 'esm',
-				sourcemap: true,
-			},
-		],
-		plugins: [resolve(), typescript()],
-	},
-	{
-		input: './src/browser.ts',
-		output: [
-			{
-				file: 'dist/langauge.js',
-				format: 'iife',
-				sourcemap: true,
-			},
-		],
-		plugins: [resolve(), typescript()],
-	},
-	{
-		input: './src/browser.ts',
-		output: [
-			{
-				file: 'dist/langauge.min.js',
-				format: 'iife',
-			},
-		],
-		plugins: [resolve(), typescript(), terser()],
-	},
-]
+const coreConfig = (minify = false) => ({
+	input: './src/browser.ts',
+	output: [
+		{
+			file: `dist/langauge${minify ? '.min' : ''}.js`,
+			format: 'iife',
+			sourcemap: true,
+		},
+	],
+	plugins: [resolve(), typescript(), minify && terser()],
+})
+
+const utilConfig = (minify = false) => ({
+	input: './src/index.ts',
+	output: [
+		{
+			file: `dist/index${minify ? '.min' : ''}.js`,
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	plugins: [resolve(), typescript(), minify && terser()],
+})
+
+const config = [coreConfig(), coreConfig(true), utilConfig(), utilConfig(true)]
 
 export default config
