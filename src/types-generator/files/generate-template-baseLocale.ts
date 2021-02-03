6024cf5198a1eca86c8a93dd21655c1f5fd77c3b
Ivan Hofer <ivan.hofer@outlook.com>
2021-02-03 07:01:27 +0100

    allow to use locales with dash in its name

--- src/types-generator/files/generate-template-baseLocale.ts
+++ src/types-generator/files/generate-template-baseLocale.ts
@@ -1,15 +1,18 @@
 import { join } from 'path'
 import { writeFileIfNotExists } from '../file-utils'
 import { GeneratorConfigWithDefaultValues } from '../generator'
+import { sanitizeLocale } from '../generator-util'
 
 const getBaseLocaleTemplate = (baseLocale: string, importType: string) => {
+	const sanitizedLocale = sanitizeLocale(baseLocale)
+
 	return `import${importType} { LangaugeBaseTranslation } from 'langauge'
 
-const ${baseLocale}: LangaugeBaseTranslation = {
+const ${sanitizedLocale}: LangaugeBaseTranslation = {
 	// TODO: your translations go here
 }
 
-export default ${baseLocale}
+export default ${sanitizedLocale}
 `
 }
 
---
+++ tests/generated/de-at/index.ts
@@ -0,0 +1,7 @@
+import type { LangaugeBaseTranslation } from 'langauge'
+
+const de_at: LangaugeBaseTranslation = {
+	// TODO: your translations go here
+}
+
+export default de_at
