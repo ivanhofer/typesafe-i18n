78439810d0941bb3b211a1038407cf76da87d5ac
Ivan Hofer <ivan.hofer@outlook.com>
2021-05-15 09:04:50 +0200

    explicitly type `locale` parameter in formatters template

--- test/generated/locales-with-dash/formatters-template.expected.ts
+++ test/generated/locales-with-dash/formatters-template.expected.ts
@@ -1,7 +1,7 @@
 import type { AsyncFormattersInitializer } from 'typesafe-i18n'
 import type { Locales, Formatters } from './types.actual'
 
-export const initFormatters: AsyncFormattersInitializer<Locales, Formatters> = async (locale) => {
+export const initFormatters: AsyncFormattersInitializer<Locales, Formatters> = async (locale: Locales) => {
 	const formatters: Formatters = {
 		// add your formatter functions here
 	}
--- test/generated/typescript-3.0/formatters-template.expected.ts
+++ test/generated/typescript-3.0/formatters-template.expected.ts
@@ -1,7 +1,7 @@
 import { AsyncFormattersInitializer } from 'typesafe-i18n'
 import { Locales, Formatters } from './types.actual'
 
-export const initFormatters: AsyncFormattersInitializer<Locales, Formatters> = async (locale) => {
+export const initFormatters: AsyncFormattersInitializer<Locales, Formatters> = async (locale: Locales) => {
 	const formatters: Formatters = {
 		// add your formatter functions here
 	}
