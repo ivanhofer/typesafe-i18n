d38591449b2f4c5415606c4615310badffc71a3f
Ivan Hofer <ivan.hofer@outlook.com>
2021-05-20 17:44:17 +0200

    write some tests for detectLocale

--- packages/locale-detector/src/detect.ts
+++ packages/locale-detector/src/detect.ts
@@ -1,30 +1,25 @@
 import { Locale } from 'packages/core/src/core'
+import { isTruthy } from 'typesafe-utils'
 
 export type LocaleDetector = () => Locale[]
 
-export const detectLocale = (
-	baseLocale: Locale,
-	availableLocales: Locale[],
-	...detectors: LocaleDetector[]
-): Locale => {
-	const lowercasedLocales = availableLocales.map((locale) => locale.toLowerCase())
-	return (
-		detectors.reduce<Locale | undefined>(
-			(prev, detector) => prev || findMatchingLocale(lowercasedLocales, detector),
-			'',
-		) || baseLocale
-	)
-}
+export const detectLocale = (baseLocale: Locale, availableLocales: Locale[], ...detectors: LocaleDetector[]): Locale =>
+	detectors.reduce<Locale | undefined>(
+		(prev, detector) => prev || findMatchingLocale(availableLocales, detector),
+		'',
+	) || baseLocale
 
 const findMatchingLocale = (availableLocales: Locale[], detector: LocaleDetector): Locale | undefined => {
 	const detectedLocales = detector().map((locale) => locale.toLowerCase())
-	const localesToMatch = [
-		...detectedLocales,
-		// also include locles without country code e.g. if only 'en-US' is detected, we should also look for 'en'
-		...(detectedLocales
-			.filter((locale) => locale.includes('-'))
-			.flatMap((locale) => locale.split('-')[0]) as Locale[]),
-	]
+	// also include locles without country code e.g. if only 'en-US' is detected, we should also look for 'en'
+	const localesToMatch = Array.from(new Set(detectedLocales.flatMap((locale) => [locale, locale.split('-')[0]])))
 
-	return localesToMatch.find((locale) => availableLocales.includes(locale))
+	const lowercasedLocales = availableLocales.map((locale) => locale.toLowerCase())
+
+	return localesToMatch
+		.map((locale) => {
+			const matchedIndex = lowercasedLocales.findIndex((l) => l === locale)
+			return matchedIndex >= 0 && availableLocales[matchedIndex]
+		})
+		.find(isTruthy)
 }
