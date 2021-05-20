8575617a0a26e72931d3aa27c241f78792c0cb6e
Ivan Hofer <ivan.hofer@outlook.com>
2021-05-20 07:26:32 +0200

    implement request-cookies detector

---
+++ packages/locale-detector/src/detectors/request-cookies.ts
@@ -0,0 +1,9 @@
+import { Request } from 'express'
+import type { Locale } from 'packages/core/src/core'
+import type { LocaleDetector } from '../detect'
+import { parseValueFromCookie } from './_cookie.util'
+
+export const initRequestCookiesDetector =
+	(req: Request, key = 'lang'): LocaleDetector =>
+		(): Locale[] =>
+			parseValueFromCookie(req?.cookies, key)
---
+++ packages/locale-detector/test/request-cookies.test.ts
@@ -0,0 +1,45 @@
+import type { Locale } from '../../core/src/core'
+
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { initRequestCookiesDetector } from '../src/detectors/request-cookies'
+import { Request } from 'express'
+
+const test = suite('detector:request-cookies')
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const testDetector = (
+	name: string,
+	cookieValue: string | undefined,
+	expected: Locale[],
+	cookieName: string | undefined = undefined,
+) =>
+	test(`request-cookies ${name}`, () => {
+		const req = { cookies: cookieValue } as Request
+		const detector = initRequestCookiesDetector(req, cookieName)
+		assert.equal(detector(), expected)
+	})
+
+testDetector('undefined', undefined, [])
+
+testDetector('empty', '', [])
+
+testDetector('single value', 'lang=de-AT', ['de-AT'])
+
+testDetector('single value with custom key', 'locale=en-US', ['en-US'], 'locale')
+
+testDetector('single value with custom key not specified', 'locale=fr', [])
+
+testDetector('single value with wrong custom key not specified', 'lang=fr', [], 'locale')
+
+testDetector('multiple values', '_ga=weTrackYouEverywhere;lang=it', ['it'])
+
+testDetector('multiple values with custom key', 'cookie=test123;user-lang=es', ['es'], 'user-lang')
+
+testDetector('multiple values without lang', 'cookie1=some-value;cookie2=another-value', [])
+
+// --------------------------------------------------------------------------------------------------------------------
+
+test.run()
