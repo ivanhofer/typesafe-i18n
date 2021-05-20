16fd2f1a06d1fdae176d41ae2b96187607be979c
Ivan Hofer <ivan.hofer@outlook.com>
2021-05-20 07:25:25 +0200

    implement query-string detector

---
+++ packages/locale-detector/src/detectors/query-string.ts
@@ -0,0 +1,16 @@
+import type { Locale } from 'packages/core/src/core'
+import type { LocaleDetector } from '../detect'
+
+export const initQueryStringDetector =
+	(key = 'lang'): LocaleDetector =>
+		(): Locale[] => {
+			const value = location?.search
+				?.slice(1)
+				.split('&')
+				.find((part) => part.startsWith(key))
+				?.split('=')[1]
+
+			return value ? [value] : []
+		}
+
+export const queryStringDetector = initQueryStringDetector()
