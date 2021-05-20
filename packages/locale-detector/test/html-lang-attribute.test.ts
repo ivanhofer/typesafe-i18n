642fd6f03567ce9f30e925f4f75c621cfc078280
Ivan Hofer <ivan.hofer@outlook.com>
2021-05-20 07:24:12 +0200

    implement html-lang-attribute detector

---
+++ packages/locale-detector/test/html-lang-attribute.test.ts
@@ -0,0 +1,50 @@
+import type { Locale } from '../../core/src/core'
+
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { htmlLangAttributeDetector } from '../src/detectors/html-lang-attribute'
+import { isNotUndefined } from 'typesafe-utils'
+
+const test = suite('detector:html-lang')
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const mockDocument = () =>
+({
+	createElement: (_tag: 'html'): HTMLHtmlElement =>
+	({
+		setAttribute: function (_key: 'lang', value: string) {
+			//@ts-ignore
+			this._lang = value
+		},
+		get lang(): string {
+			//@ts-ignore
+			return this._lang
+		},
+	} as HTMLHtmlElement),
+} as Document)
+
+const testDetector = (name: string, htmlLangValue: string | undefined, expected: Locale[]) =>
+	test(`html-lang-attribute ${name}`, () => {
+		globalThis.document = mockDocument()
+		const html = globalThis.document.createElement('html')
+		if (isNotUndefined(htmlLangValue)) {
+			html.setAttribute('lang', htmlLangValue)
+		}
+		globalThis.document = { documentElement: html as HTMLElement } as Document
+
+		assert.equal(htmlLangAttributeDetector(), expected)
+	})
+
+testDetector('undefined', undefined, [])
+
+testDetector('empty', '', [])
+
+testDetector('locale-without country', 'de', ['de'])
+
+testDetector('locale-with country', 'en-GB', ['en-GB'])
+
+// --------------------------------------------------------------------------------------------------------------------
+
+test.run()
