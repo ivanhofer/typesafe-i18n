4c287d5219b1a6539b16d72721efb535af9869af
Ivan Hofer <ivan.hofer@outlook.com>
2021-07-15 07:45:06 +0200

    chore: implement basic Angular example

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { I18nComponent } from 'src/i18n/i18n.component';
+import { I18nService } from 'src/i18n/i18n.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends I18nComponent {
   title = 'angular';
+
+  constructor(i18nService: I18nService) {
+    super(i18nService)
+
+    i18nService.initI18n('en').then(() => {
+      // eslint-disable-next-line no-console
+      console.log(this.LL.WELCOME())
+    })
+  }
 }
