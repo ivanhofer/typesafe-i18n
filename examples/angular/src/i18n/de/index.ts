fb06d03ddbe57f731fca7c95d212f3f60f5ed130
Ivan Hofer <ivan.hofer@outlook.com>
2021-08-24 18:03:34 +0200

    update to latest version

--- examples/angular/src/i18n/de/index.ts
+++ examples/angular/src/i18n/de/index.ts
@@ -1,7 +1,9 @@
 import { Translation } from '../i18n-types'
 
 const de: Translation = {
-	WELCOME: "Willkommen"
+	WELCOME: "Willkommen",
+	HI: "Hallo {name}",
+	TODAY: "Heute ist {0|weekday}",
 }
 
 export default de
--- examples/angular/src/i18n/i18n-types.ts
+++ examples/angular/src/i18n/i18n-types.ts
@@ -14,6 +14,16 @@ export type Translation = {
 	 * Welcome
 	 */
 	'WELCOME': string
+	/**
+	 * Hi {name}
+	 * @param {unknown} name
+	 */
+	'HI': RequiredParams1<'name'>
+	/**
+	 * Today is {0|weekday}
+	 * @param {Date} 0
+	 */
+	'TODAY': RequiredParams1<'0|weekday'>
 }
 
 export type TranslationFunctions = {
@@ -21,7 +31,25 @@ export type TranslationFunctions = {
 	 * Welcome
 	 */
 	'WELCOME': () => LocalizedString
+	/**
+	 * Hi {name}
+	 */
+	'HI': (arg: { name: unknown }) => LocalizedString
+	/**
+	 * Today is {0|weekday}
+	 */
+	'TODAY': (arg0: Date) => LocalizedString
 }
 
-export type Formatters = {}
+export type Formatters = {
+	'weekday': (value: Date) => unknown
+}
 
+
+type Param<P extends string> = `{${P}}`
+
+type Params1<P1 extends string> =
+	`${string}${Param<P1>}${string}`
+
+type RequiredParams1<P1 extends string> =
+	| Params1<P1>
