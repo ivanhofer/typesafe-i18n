ae70152ef9e201f423a629cb5df3260b78f80065
Ivan Hofer <ivan.hofer@outlook.com>
2021-08-25 01:50:29 +0200

    fix import path of types for `time` formatter

--- packages/formatters/src/time.ts
+++ packages/formatters/src/time.ts
@@ -1,4 +1,9 @@
-import date from './date';
+import type { FormatterFunction } from '../../core/src/core'
 
-const time = date
-export default time
+export default (
+	locale: string,
+	options: Intl.DateTimeFormatOptions = {},
+): FormatterFunction<number | Date | undefined, string> => {
+	const formatter = new Intl.DateTimeFormat(locale, options)
+	return (value) => formatter.format(value as Date)
+}
