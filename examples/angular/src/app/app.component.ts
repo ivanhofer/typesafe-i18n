91dde80d8fe023267a02bf3712f4099d042e51c3
Ivan Hofer <ivan.hofer@outlook.com>
2021-08-25 02:04:10 +0200

    remove I18nComponent

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
-import { I18nComponent } from 'src/i18n/i18n.component';
-import { I18nService } from 'src/i18n/i18n.service';
+import { TranslationFunctions } from '../i18n/i18n-types';
+import { I18nService } from '../i18n/i18n.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends I18nComponent {
+export class AppComponent {
   title = 'angular';
 
-  constructor(i18nService: I18nService) {
-    super(i18nService)
-
+  constructor(private i18nService: I18nService) {
     i18nService.initI18n('en').then(() => {
       // eslint-disable-next-line no-console
       console.log(this.LL.WELCOME())
     })
   }
+
+  get LL(): TranslationFunctions { return this.i18nService.LL }
+
 }
