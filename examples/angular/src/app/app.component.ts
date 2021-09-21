041f53b27418abf5a11006167c83f6db906f5c8b
Ivan Hofer <ivan.hofer@outlook.com>
2021-09-21 17:54:26 +0200

    chore: implement angular example

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
-import { TranslationFunctions } from '../i18n/i18n-types'
+import { localStorageDetector } from 'typesafe-i18n/detectors'
+import { Locales, TranslationFunctions } from '../i18n/i18n-types'
+import { detectLocale, locales } from '../i18n/i18n-util'
 import { I18nService } from '../i18n/i18n.service'
 
 @Component({
@@ -10,8 +12,18 @@ import { I18nService } from '../i18n/i18n.service'
 export class AppComponent {
 	title = 'angular'
 
+	selectedLocale: Locales
+
+	locales = locales
+
+	now = new Date()
+
+	nrOfApples = 1
+	nrOfBananas = 2
+
 	constructor(private i18nService: I18nService) {
-		i18nService.initI18n('en').then(() => {
+		this.selectedLocale = detectLocale(localStorageDetector)
+		i18nService.initI18n(this.selectedLocale).then(() => {
 			// eslint-disable-next-line no-console
 			console.log(this.LL.STARTUP())
 		})
@@ -19,5 +31,10 @@ export class AppComponent {
 
 	get LL(): TranslationFunctions {
 		return this.i18nService.LL
+	}
+
+	setLocale(): void {
+		this.i18nService.setLocale(this.selectedLocale)
+		localStorage.setItem('lang', this.selectedLocale)
 	}
 }
