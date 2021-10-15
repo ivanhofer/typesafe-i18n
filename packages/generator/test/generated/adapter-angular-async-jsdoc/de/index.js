5c4eb2a1b3d7b4d4150cb9581a319a806c8dff6c
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-10-15 05:51:06 +0000

    ci: version bump to 2.44.3

--- packages/generator/test/generated/adapter-angular-async-jsdoc/de/index.js
+++ packages/generator/test/generated/adapter-angular-async-jsdoc/de/index.js
@@ -10,4 +10,4 @@ const de = {
 	HI: 'Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
 }
 
-export default de
+module.exports = de
--- packages/generator/test/generated/adapter-node-async-jsdoc/en/index.js
+++ packages/generator/test/generated/adapter-node-async-jsdoc/en/index.js
@@ -10,4 +10,4 @@ const en = {
 	HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
 }
 
-export default en
+module.exports = en
