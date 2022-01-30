f787e3bdd8e829c989afa89a2e4d1eab1c4533fc
Ivan Hofer <ivan.hofer@outlook.com>
2022-01-30 13:59:42 +0100

    chore: use esbuild for parser package

---
+++ packages/parser/esbuild.ts
@@ -0,0 +1,28 @@
+import { build } from 'esbuild'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const watch = process.argv.includes('--watch')
+
+//@ts-ignore
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const getPath = (file: string) => resolve(__dirname, file)
+
+const formats = ['esm', 'cjs'] as const
+
+formats.forEach((format) =>
+	[false, true].forEach((minify) =>
+		build({
+			entryPoints: [getPath('src/index.ts')],
+			bundle: true,
+			outfile: getPath(`../../parser/index${minify ? '.min' : ''}.${format === 'esm' ? 'm' : 'c'}js`),
+			platform: 'browser',
+			format,
+			sourcemap: !minify,
+			minify,
+			watch,
+		}).catch(() => process.exit(1)),
+	),
+)
