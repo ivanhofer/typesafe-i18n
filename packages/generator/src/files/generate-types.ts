28cf5faa83d2e83621adf572eb4e029328e183be
Ivan Hofer <ivan.hofer@outlook.com>
2022-02-26 18:41:15 +0100

    split `generate-types` into smaller parts

--- packages/generator/src/files/generate-types.ts
+++ packages/generator/src/files/generate-types.ts
@@ -1,28 +1,13 @@
-import {
-	isArrayNotEmpty,
-	isNotUndefined,
-	isObject,
-	isPropertyTrue,
-	isString,
-	isTruthy,
-	not,
-	sortStringASC,
-	sortStringPropertyASC,
-	uniqueArray,
-} from 'typesafe-utils'
+import { isPropertyTrue, isTruthy } from 'typesafe-utils'
 import type { GeneratorConfigWithDefaultValues } from '../../../config/src/types'
-import { parseRawText } from '../../../parser/src/index'
-import type { ArgumentPart } from '../../../parser/src/types'
-import { BaseTranslation, isPluralPart, Locale } from '../../../runtime/src/core'
-import { partsAsStringWithoutTypes } from '../../../runtime/src/core-utils'
-import { REGEX_BRACKETS } from '../constants'
+import type { BaseTranslation, Locale } from '../../../runtime/src/core'
 import {
 	fileEndingForTypesFile,
 	importTypeStatement,
 	OVERRIDE_WARNING,
 	supportsTemplateLiteralTypes,
 } from '../output-handler'
-import { Arg, isParsedResultEntry, JsDocInfos, ParsedResult, ParsedResultEntry, Types } from '../types'
+import { isParsedResultEntry, JsDocInfos, ParsedResult } from '../types'
 import { writeFileIfContainsChanges } from '../utils/file.utils'
 import { prettify, wrapObjectKeyIfNeeded } from '../utils/generator.utils'
 import type { Logger } from '../utils/logger'
@@ -30,123 +15,10 @@ import { getTypeNameForNamespace } from '../utils/namespaces.utils'
 import { createTypeImports } from './generate-types/external-type-imports'
 import { createFormattersType } from './generate-types/formatters-type'
 import { createNamespacesTypes, validateNamespaces } from './generate-types/namespaces'
+import { parseDictionary } from './generate-types/parse-dictionary'
 import { createTranslationFunctionsType } from './generate-types/translation-functions'
 import { createTranslationType } from './generate-types/translations'
 import { flattenToParsedResultEntry, getNestedKey, wrapUnionType } from './generate-types/_utils'
-
-// TODO: refactor file into multiple smaller files
-
-// --------------------------------------------------------------------------------------------------------------------
-
-const parseTranslations = (
-	translations: BaseTranslation | BaseTranslation[] | Readonly<BaseTranslation> | Readonly<BaseTranslation[]>,
-	logger: Logger,
-	parentKeys = [] as string[],
-): ParsedResult[] =>
-	isObject(translations)
-		? Object.entries(translations).map(([key, text]) => {
-				if (isString(text)) {
-					return parseTranslationEntry([key, text], logger, parentKeys) as ParsedResultEntry
-				}
-				return { [key]: parseTranslations(text, logger, [...parentKeys, key]) } as ParsedResult
-		  })
-		: []
-
-const parseTypesFromSwitchCaseStatement = (formatters: string[] | undefined) => {
-	if (!formatters?.length) return undefined
-
-	const formatter = formatters[0] as string
-	if (!formatter.startsWith('{')) return undefined
-
-	const keys = formatter
-		.replace(REGEX_BRACKETS, '')
-		.split(',')
-		.map((s) => s.split(':'))
-		.map(([key]) => key?.trim())
-		.sort()
-
-	return keys.map((key) => (key === '*' ? 'string' : `'${key}'`)).join(' | ')
-}
-
-const parseTranslationEntry = (
-	[key, text]: [string, string],
-	logger: Logger,
-	parentKeys: string[],
-): ParsedResult | null => {
-	const parsedParts = parseRawText(text, false)
-	const textWithoutTypes = partsAsStringWithoutTypes(parsedParts)
-
-	const parsedObjects = parsedParts.filter(isObject)
-	const argumentParts = parsedObjects.filter<ArgumentPart>(not(isPluralPart))
-	const pluralParts = parsedObjects.filter(isPluralPart)
-
-	const args: Arg[] = []
-	const types: Types = {}
-
-	argumentParts.forEach(({ k, n, i, f }) => {
-		args.push({ key: k, formatters: f, optional: n })
-		const type = i ?? parseTypesFromSwitchCaseStatement(f)
-		types[k] = {
-			types: uniqueArray([...(types[k]?.types || []), type]).filter(isNotUndefined),
-			optional: types[k]?.optional || n,
-		}
-	})
-
-	pluralParts.forEach(({ k }) => {
-		if (!types[k]?.types.length) {
-			// if key has no types => add types that are valid for a PluralPart
-			types[k] = { types: ['string', 'number', 'boolean'] }
-			if (!args.find(({ key }) => key === k)) {
-				// if only pluralPart exists => add it as argument
-				args.push({ key: k, formatters: [], pluralOnly: true })
-			}
-		}
-	})
-
-	// add 'unknown' if argument has no type
-	Object.keys(types).forEach((key) => {
-		if (!types[key]?.types.length) {
-			types[key] = { types: ['unknown'], optional: types[key]?.optional }
-		}
-	})
-
-	args.sort(sortStringPropertyASC('key'))
-
-	const isValid = validateTranslation(key, types, logger)
-
-	if (!isValid) return null
-
-	return { key, text, textWithoutTypes, args, types, parentKeys }
-}
-
-const validateTranslation = (key: string, types: Types, logger: Logger): boolean => {
-	const base = `translation '${key}' =>`
-
-	if (key.includes('.')) {
-		logger.error(
-			`${base} key can't contain the '.' character. Please remove it. If you want to nest keys, you should look at https://github.com/ivanhofer/typesafe-i18n#nested-translations`,
-		)
-		return false
-	}
-
-	const argKeys = Object.keys(types).sort(sortStringASC)
-	if (isArrayNotEmpty(argKeys) && !isNaN(+argKeys[0])) {
-		let expectedKey = '0'
-		for (const argKey of argKeys) {
-			if (argKey !== expectedKey) {
-				const info = isNaN(+argKey)
-					? `You can't mix keyed and index-based arguments.`
-					: `Make sure to not skip an index for your arguments.`
-
-				logger.error(`${base} argument {${expectedKey}} expected, but {${argKey}} found.`, info)
-				return false
-			}
-			expectedKey = (+argKey + 1).toString()
-		}
-	}
-
-	return true
-}
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -175,8 +47,6 @@ const createJsDocsMapping = (parsedTranslations: ParsedResult[]) => {
 
 // --------------------------------------------------------------------------------------------------------------------
 
-// --------------------------------------------------------------------------------------------------------------------
-
 const doesATranslationContainParams = (p: ParsedResult[]): boolean =>
 	p.some((parsedResult) =>
 		isParsedResultEntry(parsedResult)
@@ -191,7 +61,7 @@ const getTypes = (
 	const usesNamespaces = !!namespaces.length
 	validateNamespaces(translations, namespaces)
 
-	const parsedTranslations = parseTranslations(translations, logger).filter(isTruthy)
+	const parsedTranslations = parseDictionary(translations, logger).filter(isTruthy)
 
 	const externalTypeImports = createTypeImports(parsedTranslations, typesTemplateFileName)
 
