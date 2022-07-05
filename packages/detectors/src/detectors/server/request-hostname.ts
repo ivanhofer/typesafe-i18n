6b41dda5867a438c36e62bcb7d2480bdce3a6e5a
Ivan Hofer <ivan.hofer@outlook.com>
2022-07-05 17:34:01 +0200

    add regex info

--- packages/detectors/src/detectors/server/request-hostname.ts
+++ packages/detectors/src/detectors/server/request-hostname.ts
@@ -5,12 +5,13 @@ type Request = {
 	hostname: string
 }
 
-const HOST_LANG_REGEX = /^((?:\w{2,3}(?:-\w{3})?)(?:-\w{4})?(?:-\w{2}|-\d{3})?(?:-[\w\d]{5,8}|-\d[\w\d]{3})*)\./
+// matches language tags as per RFC 5646
+const REGEX_HOST_LANG = /^((?:\w{2,3}(?:-\w{3})?)(?:-\w{4})?(?:-\w{2}|-\d{3})?(?:-[\w\d]{5,8}|-\d[\w\d]{3})*)\./
 
 export const initRequestHostnameDetector =
 	(req: Request): LocaleDetector =>
 	(): Locale[] => {
-		const match = req?.hostname?.match(HOST_LANG_REGEX)
+		const match = req?.hostname?.match(REGEX_HOST_LANG)
 
 		if (match && match[1]) {
 			return [match[1]]
