b1af55f4c89e72c014891c2f42df3bfc19a5b0b8
Matthew Gibbons <mhgibbons@me.com>
2022-07-05 11:44:49 +0100

    Move regex to root. Extend regex re: RFC 5646.

--- packages/detectors/src/detectors/server/request-hostname.ts
+++ packages/detectors/src/detectors/server/request-hostname.ts
@@ -5,10 +5,12 @@ type Request = {
 	hostname: string
 }
 
+const HOST_LANG_REGEX = /^((?:\w{2,3}(?:-\w{3})?)(?:-\w{4})?(?:-\w{2}|-\d{3})?(?:-[\w\d]{5,8}|-\d[\w\d]{3})*)\./
+
 export const initRequestHostnameDetector =
 	(req: Request): LocaleDetector =>
 	(): Locale[] => {
-		const match = req?.hostname?.match(/^(?:([a-z]{2}(?:-[A-Z]{2})?)\.)/)
+		const match = req?.hostname?.match(HOST_LANG_REGEX)
 
 		if (match && match[1]) {
 			return [match[1]]
