99a6ff13c811b518564b09cffd87dc8eb38c2f0a
jesperp <376921+jesperp@users.noreply.github.com>
2022-07-25 14:45:15 +0200

    isomorphic dirname

--- packages/generator/test/generated/update.ts
+++ packages/generator/test/generated/update.ts
@@ -1,17 +1,21 @@
 // this file will replace all the expected.js files with their _actual equivalents
 
 import { readFileSync, writeFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import glob from 'tiny-glob/sync.js'
 
 console.log(`
 updating generated files ...`
 )
 
-const __dirname = new URL('.', import.meta.url).pathname;
+const cwd = typeof __dirname !== 'undefined'
+  ? __dirname
+  : dirname(fileURLToPath(import.meta.url))
 
-glob('**/*.actual.*', { cwd: __dirname }).forEach((file) => {
-	const actual = readFileSync(`${__dirname}/${file}`, 'utf-8')
-	writeFileSync(`${__dirname}/${file.replace('.actual.', '.expected.')}`, actual)
+glob('**/*.actual.*', { cwd }).forEach((file) => {
+	const actual = readFileSync(`${cwd}/${file}`, 'utf-8')
+	writeFileSync(`${cwd}/${file.replace('.actual.', '.expected.')}`, actual)
 })
 
 console.log('... completed')
