4976cf6d5809da828341ca3ddca45aa157f6e881
Hofer Ivan <ivan.hofer@outlook.com>
2023-02-06 08:37:28 +0100

    chore: update `@typesafe-i18n/importer` example (#591)
    Co-authored-by: ivanhofer <ivanhofer@users.noreply.github.com>

--- packages/importer/example/src/i18n/i18n-util.ts
+++ packages/importer/example/src/i18n/i18n-util.ts
@@ -5,6 +5,7 @@ import { i18n as initI18n, i18nObject as initI18nObject, i18nString as initI18nS
 import type { LocaleDetector } from 'typesafe-i18n/detectors'
 import type { LocaleTranslationFunctions, TranslateByString } from 'typesafe-i18n'
 import { detectLocale as detectLocaleFn } from 'typesafe-i18n/detectors'
+import { initExtendDictionary } from 'typesafe-i18n/utils'
 import type { Formatters, Locales, Namespaces, Translations, TranslationFunctions } from './i18n-types'
 
 export const baseLocale: Locales = 'en'
@@ -20,11 +21,14 @@ export const namespaces: Namespaces[] = [
 
 export const isLocale = (locale: string): locale is Locales => locales.includes(locale as Locales)
 
-export const isNamespace = (namespace: string): namespace is Namespaces => namespaces.includes(namespace as Namespaces)
+
+	export const isNamespace = (namespace: string): namespace is Namespaces => namespaces.includes(namespace as Namespaces)
 
 export const loadedLocales: Record<Locales, Translations> = {} as Record<Locales, Translations>
 
 export const loadedFormatters: Record<Locales, Formatters> = {} as Record<Locales, Formatters>
+
+export const extendDictionary = initExtendDictionary<Translations>()
 
 export const i18nString = (locale: Locales): TranslateByString => initI18nString<Locales, Formatters>(locale, loadedFormatters[locale])
 
